752c8d2b3f07b9de622abbda730038118a4791c0
wangjie <728199993@qq.com>
2018-04-19 11:41:52 +0800

    first commit

--- src/main/java/com/hello/conroller/HelloController.java
+++ src/main/java/com/hello/conroller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+//入口
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> string() {
         return new ResponseEntity<String>("hello", HttpStatus.OK);
